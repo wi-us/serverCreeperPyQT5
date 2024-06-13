dff9db6bd78168a1fd7156f110cd45827b006325
wi-us <92710158+wi-us@users.noreply.github.com>
2024-06-13 13:51:17 +0200

    Delete py directory

--- py/swagger_client/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.booking_api import BookingApi
-from swagger_client.api.table_api import TableApi
-from swagger_client.api.user_api import UserApi
